2208500bdb381db0a7d9181147fad5d7c8e3dc61
moharick <moharick@gmail.com>
2019-07-21 20:00:57 +0300

    add functions to the file.

---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+    $("#hide-1").hide();
+    $(".toggle1, #hide-1").click(function(){
+       $(".toggle1").hide(1000); 
+        $("#hide-1").show(1000);
+});
+
+$("#hide-1").click(function(){
+    $(".toggle1").slideDown();
+    $("#hide-1").hide(1000);
+})
+});
+$(document).ready(function(){
+    $("#hide-2").hide();
+    $(".toggle2").click(function(){
+       $(".toggle2").hide(1000); 
+        $("#hide-2").show(1000);
+});
+$("#hide-2").click(function(){
+    $(".toggle2").slideDown();
+    $("#hide-2").hide(1000);
+})
+});
+$(document).ready(function(){
+    $("#hide-3").hide();
+    $(".toggle3").click(function(){
+       $(".toggle3").hide(1000); 
+        $("#hide-3").show(1000);
+});
+$("#hide-3").click(function(){
+    $(".toggle3").slideDown();
+    $("#hide-3").hide(1000);
+})
+});
+$(document).ready(function() {
+    $('#work1').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
+$(document).ready(function() {
+    $('#work2').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
+$(document).ready(function() {
+    $('#work3').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
+$(document).ready(function() {
+    $('#work4').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
+$(document).ready(function() {
+    $('#work5').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
+$(document).ready(function() {
+    $('#work6').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
+$(document).ready(function() {
+    $('#work7').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
+$(document).ready(function() {
+    $('#work8').hover(function(){
+		$('.image-cap',this).slideToggle('slow');
+	}, function(){
+		$('.image-cap',this).slideToggle('slow');
+    });
+});
